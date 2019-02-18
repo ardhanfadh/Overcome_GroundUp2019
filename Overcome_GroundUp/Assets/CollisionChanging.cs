a12656570cbe8c4598e375959be7ee1a6f04a0e2
Ardhan <dandut88@gmail.com>
2019-02-18 00:16:20 +0000

    Initial Integration

---
+++ Overcome_GroundUp/Assets/CollisionChanging.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChanging : MonoBehaviour {
+    public BoxCollider2D flyingCollider;
+    public BoxCollider2D landedCollider;
+
+	// Use this for initialization
+	void Start () {
+        flyingCollider.isTrigger = false;
+        landedCollider.isTrigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
